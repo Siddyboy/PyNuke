dd13d2bdd44631c880ba54185d2e01dc79f8c3a7
Simon Kingsley <simon.kingsley@oxinst.com>
2017-03-21 16:34:52 +0000

    Prepare material.py for use by no_effect.
    Thinking about how to pass material selectrion argument into module. And
    how to handle exceptions.

--- material.py
+++ material.py
@@ -4,39 +4,39 @@ Created on 7 Jan 2014
 
 This module contains data for specific nuclear orientation materials.
 
-In this order:
+The dictionary keys are:
 
-* Active nuclei species for gamma ray production.
-* Host matrix (non-radioactive).
-* Nuclear magnetic dipole moment in units of nuclear magnetons [1].
-* Hyperfine field in Tesla [2].
-* Nuclear spin angular momentum, dimensionless [3].
-* Nuclear spin parity [3].
-* U2F2 coefficients, dimensionless. [2]
-* U4F4 coefficients, dimensionless. [2]
-* epsilon. Fraction deviation from full magnetic saturation. [2]
+* 'active': The active nuclei species for gamma ray production.
+* 'host': The host matrix (non-radioactive).
+* 'moment': The nuclear magnetic dipole moment in units of nuclear 
+     magnetons [1].
+* 'moment_old': An alternative, older value for 'moment'. See ref [2].
+* 'field': The hyperfine field in Tesla [2].
+* 'spin': The nuclear spin angular momentum, dimensionless [3].
+* 'parity': The nuclear spin parity [3].
+* 'U2F2': Coefficients, dimensionless. [2]
+* 'U4F4': Coefficients, dimensionless. [2]
+* 'epsilon': Fraction deviation from full magnetic saturation. [2]
 
 '''
-
+    
 mat_cobalt = {
     'active': "cobalt-60",
     'host': "cobalt (hcp) single crystal",
     'moment': 3.799,
-# Alternative, older value [2]. SCJK 7 Jan 2014
     'moment_old': 3.754,  
     'field': -21.92,
     'spin': 5,
     'parity': "+",
-    'U2F2': -0.42056,
+     'U2F2': -0.42056,
     'U4F4': -0.24280,
     'epsilon': 0.0
-     }
+    }
     
 mat_manganese = {
     'active': "manganese-54",
     'host': "nickel",
     'moment': 3.2819,
-# Alternative older value [2]. SCJK 7 Jan 2014
     'moment_old': 3.302,
     'field': -32.56,
     'spin': 3,
@@ -47,14 +47,21 @@ mat_manganese = {
     'epsilon': 0.0,  
     }
 
-#    active = "cobalt-60"
-#    host = "cobalt (hcp) single crystal"
-#    moment = 3.799
-#    # Alternative older value [2]. SCJK 7 Jan 2014
-#    # moment = 3.754
-#    field = -21.92
-#    spin = 5
-#    parity = "+"
-#    U2F2 = -0.42056
-#    U4F4 = -0.24280
-#    epsilon = 0.0
+def main(m):
+    """
+    Needs Google docstring!!! With doctests
+    
+    """
+    if m == 'cobalt':
+        mat = mat_cobalt
+    elif m == 'manganese':
+        mat = mat_manganese
+    else:
+        raise ValueError("Material selection not valid.")
+    
+    print("mat is", mat)
+    return mat
+    
+if __name__ == '__main__':
+    main(None)
+    
