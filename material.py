f5e5001b218de426ca33a13d0ff1cc618ca8aec0
Simon Kingsley <simon.kingsley@oxinst.com>
2017-03-27 14:30:49 +0100

    Almost done with this file.
    Probably ok for now.

--- material.py
+++ material.py
@@ -1,39 +1,25 @@
-"""This module contains data in dictionaries for specific nuclear orientation
-thermometry materials.
+"""Contains materials data for specific nuclear orientation thermometry
+materials.
 
 The dictionary keys are:
-
 'active': The active nuclei species for gamma ray production.
-
 'host': The host matrix (non-radioactive).
-
-'moment': The nuclear magnetic dipole moment in units of nuclear 
+'moment': The nuclear magnetic dipole moment in units of nuclear
      magnetons [1].
-
 'moment_old': An alternative, older value for 'moment'. See ref [2].
-
 'field': The hyperfine field in Tesla [2].
-
 'spin': The nuclear spin angular momentum, dimensionless [3].
-
 'parity': The nuclear spin parity [3].
-
 'U2F2': Coefficients, dimensionless. [2]
-
 'U4F4': Coefficients, dimensionless. [2]
-
 'epsilon': Fraction deviation from full magnetic saturation. [2]
 """
 
-# TODO(SCJK): Just put this guff in main module - see http://stackoverflow.com/questions/1523427/what-is-the-common-header-format-of-python-files
-__author__ = "Simon C. J. Kingsley"
-__email__ = "simon.kingsley@oxinst.com"
-      
 mat_cobalt = {
     'active': "cobalt-60",
     'host': "cobalt (hcp) single crystal",
     'moment': 3.799,
-    'moment_old': 3.754,  
+    'moment_old': 3.754,
     'field': -21.92,
     'spin': 5,
     'parity': "+",
@@ -41,7 +27,7 @@ mat_cobalt = {
     'U4F4': -0.24280,
     'epsilon': 0.0
 }
-    
+
 mat_manganese = {
     'active': "manganese-54",
     'host': "nickel",
@@ -52,16 +38,17 @@ mat_manganese = {
     'parity': "+",
     'U2F2': -0.49486,
     'U4F4': -0.44669,
-# TODO(SCJK): Not sure epsilon should be zero for 54Mn. Probably needs more
-# research.
-    'epsilon': 0.0,  
+    # TODO(SCJK): Not sure epsilon should be zero for 54Mn. Probably needs more
+    # research.
+    'epsilon': 0.0,
 }
+
 
 # TODO(SCJK): I'm not sure that the following code is strictly necessary. Is
 # it redundant?
 def main():
     pass
 
+
 if __name__ == "__main__":
     main()
-    
